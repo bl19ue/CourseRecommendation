bbf220a550aaf666583aefb973e23422332df48d
Sumit Valecha <svalecha91@gmail.com>
2015-04-21 17:06:15 -0700

    user route started

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var Q = require('q');
 
 var UserSchema = mongoose.model('Users');
-
+var CourseSchema = mongoose.model('Courses');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/user/:userid/courses', function(req, res){
+	var userid = req.params.userid;
+	
+	
+});
 
+function findUser(userid){
+	var deferred = Q.defer();
+	UserSchema.findOne({userid: userid}, function(err, user){
+		if(err){
+			deferred.reject(err);
+		}
+		else{
+			if(user){
+				deferred.resolve(user);
+			}
+			else{
+				deferred.reject("No user found");
+			}
+		}
+	});
+
+	return deferred.promise;
+}
 
 module.exports = router;
