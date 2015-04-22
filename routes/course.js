90c71ee01a454cea7575b3bd706c016987afae06
Sudhakar Kamanboina <sudhakar.kamanboina@gmail.com>
2015-04-21 18:23:26 -0700

    populated course into mongoDB

--- routes/course.js
+++ routes/course.js
@@ -2,7 +2,48 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-var UserSchema = mongoose.model('Courses');
+var CourseSchema = mongoose.model('Courses');
+
+var request = require("request")
+
+var url = "https://api.coursera.org/api/catalog.v1/courses?fields=aboutTheCourse";
+
+
+
+router.get('/', function(req, res) {
+   var dataurl ='';
+   console.log("in data url");
+   		request({
+				    url: url,
+				    json: true
+				}, function (error, response, body) {
+
+				    if (!error && response.statusCode === 200) {
+				    	for(var i=0;i<body.elements.length;i++){
+				    		//console.log(body.elements[i]); // Print the json response
+				    		var newData= new CourseSchema({id:body.elements[i].id,shortName:body.elements[i].shortName,name:body.elements[i].name,
+				    		about:body.elements[i].aboutTheCourse.split(" ")});
+				    		newData.save(function(err) {
+								  if (err) throw err;
+
+								  console.log('Added!' + i);
+								});
+
+				    	}
+
+				    	res.end();
+				        
+				    }
+				})
+		//
+	});
+
+	router.get('/test', function(req, res) {
+		var skill = "java";
+   		CourseSchema.find();
+		
+	}); 
+
 
 //REST APIs here
 //e.g. router.get('/courses', function(req, res){});
