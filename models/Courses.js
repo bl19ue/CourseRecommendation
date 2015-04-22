361b1a7c0078e4e018c581e265dee536f15a6196
Sudhakar Kamanboina <sudhakar.kamanboina@gmail.com>
2015-04-21 20:39:29 -0700

    Search functionality added

--- models/Courses.js
+++ models/Courses.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 var CourseSchema = new mongoose.Schema({
-	
+	id:String,
+	shortName:String,
+	name:String,
+	about: [{type: String}]
 });
 
 mongoose.model('Courses', CourseSchema, 'Courses');
--- routes/course.js
+++ routes/course.js
@@ -6,13 +6,13 @@ var CourseSchema = mongoose.model('Courses');
 
 var request = require("request")
 
-var url = "https://api.coursera.org/api/catalog.v1/courses?fields=aboutTheCourse";
+var url = "https://api.coursera.org/api/catalog.v1/courses?fields=aboutTheCourse,language";
 
 
 
 router.get('/', function(req, res) {
    var dataurl ='';
-   console.log("in data url");
+   console.log("in data url "+url);
    		request({
 				    url: url,
 				    json: true
@@ -22,7 +22,7 @@ router.get('/', function(req, res) {
 				    	for(var i=0;i<body.elements.length;i++){
 				    		//console.log(body.elements[i]); // Print the json response
 				    		var newData= new CourseSchema({id:body.elements[i].id,shortName:body.elements[i].shortName,name:body.elements[i].name,
-				    		about:body.elements[i].aboutTheCourse.split(" ")});
+				    		about:body.elements[i].aboutTheCourse});
 				    		newData.save(function(err) {
 								  if (err) throw err;
 
@@ -39,8 +39,14 @@ router.get('/', function(req, res) {
 	});
 
 	router.get('/test', function(req, res) {
-		var skill = "java";
-   		CourseSchema.find();
+		var skill = "Java";
+   		CourseSchema.find({name: { $regex: skill} }, function(err, courses) {
+   			console.log(courses);
+   			res.json({
+
+   				data:courses
+   			});
+   		});
 		
 	}); 
 
