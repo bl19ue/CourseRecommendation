c4ddb2452d07f5cbd4cda627befe0b7dec8526c0
Sumit Valecha <svalecha91@gmail.com>
2015-04-22 19:43:19 -0700

    LinkedIn Working with UI

--- routes/course.js
+++ routes/course.js
@@ -11,44 +11,49 @@ var url = "https://api.coursera.org/api/catalog.v1/courses?fields=aboutTheCourse
 
 
 router.get('/', function(req, res) {
-   var dataurl ='';
-   console.log("in data url "+url);
-   		request({
-				    url: url,
-				    json: true
-				}, function (error, response, body) {
+	var dataurl ='';
+	console.log("in data url "+url);
+	request({
+		url: url,
+		json: true
+	}, function (error, response, body) {
 
-				    if (!error && response.statusCode === 200) {
-				    	for(var i=0;i<body.elements.length;i++){
-				    		//console.log(body.elements[i]); // Print the json response
-				    		var newData= new CourseSchema({id:body.elements[i].id,shortName:body.elements[i].shortName,name:body.elements[i].name,
-				    		about:body.elements[i].aboutTheCourse});
-				    		newData.save(function(err) {
-								  if (err) throw err;
+		if (!error && response.statusCode === 200) {
+			for(var i=0;i<body.elements.length;i++){
+				//console.log(body.elements[i]); // Print the json response
+				console.log(body.elements[i].language);
+				if(body.elements[i].language == "en"){
+					var newData= new CourseSchema({id:	body.elements[i].id,
+												   shortName:	body.elements[i].shortName,
+												   name:	body.elements[i].name,
+												   about:	body.elements[i].aboutTheCourse.split(" ")});
+					newData.save(function(err) {
+						if (err) throw err;
+						
+						
+					});
+					//console.log('Added!' + i);
+				}
 
-								  console.log('Added!' + i);
-								});
+			}
 
-				    	}
+			res.end();
 
-				    	res.end();
-				        
-				    }
-				})
-		//
+		}
+	})
+	//
+});
+
+router.get('/test', function(req, res) {
+	var skill = "Java";
+	CourseSchema.find({$or: [{name: {$regex: skill}}, {about: {$regex: skill}}] } , function(err, courses) {
+		console.log(courses);
+		res.json({
+			data:courses
+		});
 	});
 
-	router.get('/test', function(req, res) {
-		var skill = "Java";
-   		CourseSchema.find({name: { $regex: skill} }, function(err, courses) {
-   			console.log(courses);
-   			res.json({
-
-   				data:courses
-   			});
-   		});
-		
-	}); 
+}); 
 
 
 //REST APIs here
--- routes/index.js
+++ routes/index.js
@@ -8,31 +8,31 @@ var CourseSchema = mongoose.model('Courses');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
-router.get('/user/:userid/courses/:numresult', function(req, res){
-	var userid = req.params.userid;
-	var num_result = req.params.numresult;
-	
-	findUser().then(function(user){
-		
+router.get('/user/:name/courses', function(req, res){
+	var name = req.params.name;
+	var num_result = 2;
+
+	findUser(name).then(function(user){
+		console.log(user.name);
 		var skills = user.skills;
-		
-		getCourses(skills, num_result).then(function(result){
-			
-			respondData(res, result);
-			
+		if(skills == null){ return; }
+		getCourses(skills, num_result).then(function(results){
+			res.json({data:results});
+			//respondData(res, results);
+
 		}).fail(function(err){
-			
+
 			console.log(err);
-			
+
 		});
-		
+
 	}).fail(function(err){
-		
+
 		console.log(err);
-		
+
 	});
 });
 
@@ -43,22 +43,56 @@ function respondData(res, data){
 	});
 }
 
+function findCourse(skill, max_results){
+	var deferred = Q.defer();
+	CourseSchema.find({$or: [{name: skill}, {about: skill}] }, {id:1, shortName:1, name:1} , function(err, courses) {
+		if(err){
+			deferred.reject(err);
+		}
+		else{
+			var obj = {
+				
+			}
+			//console.log(courses);
+			deferred.resolve(courses);
+		}
+	}).limit(max_results);
+
+	return deferred.promise;
+} 
+
 function getCourses(skills, max_results){
 	var deferred = Q.defer();
 	var results = [];
-	
+	var lengthOfSkills = skills.length;
+	var count = 0;
 	skills.forEach(function(skill){
 		//Get the courses for this skill with limit set to [max_results]
 		
+		findCourse(skill, max_results).then(function(courses){
+			console.log('skill:'+skill);
+			results.push({
+				skill : skill,
+				courses : courses
+			});
+			if(count == lengthOfSkills - 1){
+				deferred.resolve(results);			
+			}
+			count++;
+		}).fail(function(err){
+			console.log(err);
+		});
+		//deferred.resolve(results);
 		//results.push(COURSES);
 	});
-	deferred.resolve(results);
+	console.log('check results:' + results[0]);
+	
 	return deferred.promise;
 }
 
-function findUser(userid){
+function findUser(name){
 	var deferred = Q.defer();
-	UserSchema.findOne({userid: userid}, function(err, user){
+	UserSchema.findOne({name: name}, function(err, user){
 		if(err){
 			deferred.reject(err);
 		}
