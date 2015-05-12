e56a0f70d5269119e412f3b4c8d77d281905a5f2
Sudhakar Kamanboina <sudhakar.kamanboina@gmail.com>
2015-05-11 20:17:47 -0700

    Resolved email module version issue and added email functionality

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mail = require('../utils/mailer.js');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
+	new mail().sendMail('xyz@gmail.com', 'TEST', 'TEXT');
   res.send('respond with a resource');
 });
 
