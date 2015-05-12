0bcab1aa8d57cee09537d9e662e3de2f2f79c934
Piyush Bansal <piyush043@gmail.com>
2015-05-11 20:01:03 -0700

    Recommend Course List Logic and UI

--- models/Courses.js
+++ models/Courses.js
@@ -7,7 +7,7 @@ var CourseSchema = new mongoose.Schema({
 	about: [{type: String}],
 	url: String,
 	skills:[],
-	level: {type: Number, enum: [1,2,3]}
+	level: {type: Number, enum: [1,2,3]} //1: begineer, 2:intermediate, 3: expert
 });
 
 mongoose.model('Courses', CourseSchema, 'Courses');
--- routes/rules.js
+++ routes/rules.js
@@ -6,27 +6,34 @@ var q = require('q');
 var CourseSchema = mongoose.model('Courses');
 
 router.post('/recommend', function(req, res){
-	console.log("Recommending Courses");
+	console.log("----------------Recommending Courses------------------");
 	reqData = JSON.parse(req.body.data)
 	reqDataLength = reqData.length;
 	//console.log(reqData);
 	//console.log(reqData.length);
 	var data = JSON.stringify(reqData);
+	var result;
 	if(reqDataLength<5){
-		computeSkillArray(data, reqData.length).then(function(skillArray){
-			console.log(skillArray);
-			res.json({
-				type: true,
-				data: "Response"
+		computeSkillArray(data, reqDataLength).then(function(skillArray){
+			findCoursesforSkill(skillArray).then(function(result){
+				console.log("===============Result============");	
+				console.log("Results found: " + result.length);
+				res.json({
+					type: true,
+					data: "Response"
+				});
 			});
 		});
 
 	}else{
-		computeSkillArray(data, 5).then(function(skillArray){
-			console.log(skillArray);
-			res.json({
-				type: true,
-				data: "Response"
+		computeSkillArray(data, reqDataLength).then(function(skillArray){
+			findCoursesforSkill(skillArray).then(function(result){
+				console.log("===============Result============");	
+				console.log("Results found: " + result.length);
+				res.json({
+					type: true,
+					data: result
+				});
 			});
 		});
 	}
@@ -35,9 +42,9 @@ router.post('/recommend', function(req, res){
 });
 
 var computeSkillArray = function(reqData, len){
-	console.log("computeSkillArray(" + reqData + "," + len + ")");
+	//console.log("computeSkillArray(" + reqData + "," + len + ")");
 	var skillData = JSON.parse(reqData);
-	console.log(skillData);
+	// console.log(skillData);
 	//console.log(skillData[0]);
 	var skillArray = [];
 	var i =0;
@@ -45,31 +52,105 @@ var computeSkillArray = function(reqData, len){
 
 	for(i=0; i<len; i){
 		skill = skillData[i];
-	// skillData.foreach(function(skill){
-		console.log(skill);
+		// console.log(skill);
 		skillCount = skill.count;
-		console.log(skill.count);
+		// console.log(skill.count);
+		var level;
 		if(skillCount>=5){
 			console.log(skill.skill + ' : ' + 'Expert');
 		}else if(skillCount<5 && skillCount > 2){
 			console.log(skill.skill + ' : ' + 'Intermediate');
-			skillArray.push({'skill' : skill.skill, 'level' : 'Intermediate'});
+			level = 3;
+			skillArray.push({'skill' : skill.skill, 'level' : level});
 			i++;
 		}else if(skillCount==1){
+			level = 2;
 			console.log(skill.skill + ' : ' + 'Beginner');
-			skillArray.push({'skill' : skill.skill, 'level' : 'Beginner'});
+			skillArray.push({'skill' : skill.skill, 'level' : level});
 			i++;
 		}else{
-			console.log(skill.skill + ' : ' + 'Beginner');
-			skillArray.push({'skill' : skill.skill, 'level' : 'Beginner'});
+			level = 1;
+			console.log(skill.skill + ' : ' + 'None');
+			skillArray.push({'skill' : skill.skill, 'level' : level});
 			i++;
-		}
-	// });
+		}	
 	}
-	
-	deferred.resolve(skillArray);
 
+	deferred.resolve(skillArray);
+	
+	console.log("---------------------------skillArray--------------------------");
+	console.log(JSON.stringify(skillArray));
+	
 	return deferred.promise;
+}
+
+function findCoursesforSkill(skillArray){
+	var deferred2 = q.defer();
+	var result = [];
+	var count = 0;
+	var recommendedCoursesLength = 0;
+	skillArray.forEach(function(skill){
+		console.log('Finding level#:'+  skill.level + ' courses for skill: ' + skill.skill);
+		skillName = skill.skill.toLowerCase();
+		CourseSchema.find({'skills': skillName, 'level': skill.level}, function(err, courses){
+			//console.log(courses);
+			if(err){
+				console.log("Error: " + err);
+				deferred2.reject(err);
+			}else{
+				if(courses.length!=0){
+					recommendedCoursesLength += courses.length;
+					console.log("---------courses found for " + skill.skill + " skill.");
+					result.push({'skill' : skill.skill, 'level' : skill.level, 'courses': JSON.stringify(courses)});					
+				}else{
+					console.log("No course found for "  + skill.skill);
+					//deferred2.reject("No course found");
+				}
+			}
+			if(count == skillArray.length - 1){
+				if(recommendedCoursesLength < 10){
+					findMoreCourses(skillArray, result, recommendedCoursesLength).then(function(result){
+						deferred2.resolve(result);
+					});
+				}else{
+					deferred2.resolve(result);
+				}
+			}
+			count++;
+		});
+	});
+
+	return deferred2.promise;
+}
+
+var findMoreCourses = function(skillArray, result, recommendedCoursesLength){
+
+	console.log("---------------------Finding more courses-----------------------");
+	var deferred3 = q.defer();
+	var count = 0;
+	skillArray.forEach(function(skill){
+		skillName = skill.skill.toLowerCase();
+		CourseSchema.find({'skills': skillName}, function(err, courses){
+			if(err){
+				console.log("Error: " + err);
+				deferred3.reject(err);
+			}else{
+				if(courses.length!=0){
+					console.log("recommendedCoursesLength = " +recommendedCoursesLength );
+					recommendedCoursesLength += courses.length;
+					console.log("---------courses found for " + skill.skill + " skill.");
+					result.push({'skill' : skill.skill, 'level' : skill.level, 'courses': JSON.stringify(courses)});					
+				}else{
+					console.log("No course found for "  + skill.skill);
+					//deferred2.reject("No course found");
+				}
+			}
+			if(recommendedCoursesLength > 10){
+				deferred3.resolve(result);
+			}
+		});
+	});
+	return deferred3.promise;
 }
 
 module.exports = router;
